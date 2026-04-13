2b1b7f1e5f4ac3aa3ccecacb5ed740fcf36bbabd
Andrey Ovechkin <andryu.ovechkin@gmail.com>
2026-04-13 20:19:58 +0300

    fix: use Path for correct filename parsing

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,40 @@ mod zip_lib;
 mod tree_lib;
 mod huffman_utils;
 use std::env;
+use std::path::Path;
+
 
 fn parser(args: Vec<String>) -> Result<String, String> {
     let output_filename: String;
+    
     if args.len() == 3 {
-        let last_point = match args[2].rfind('.') {
-            Some(position) => position,
-            None => return Err("Path to file doesn't contain a file extension".to_string())
+        let input_path = &args[2];
+        let path = Path::new(input_path);
+        
+        let stem = path
+            .file_stem()
+            .and_then(|s| s.to_str())
+            .ok_or_else(|| "Invalid file stem (not valid UTF-8)".to_string())?;
+        
+        let ext = path
+            .extension()
+            .and_then(|e| e.to_str())
+            .map(|e| format!(".{}", e))
+            .unwrap_or_default();
+        
+        let name = match args[1].as_str() {
+            "zip" => "_zipped",
+            "unzip" => "_unzipped",
+            _ => return Err("First argument should be \"zip\" or \"unzip\".".to_string()),
         };
-
-        let name: String;
-        if args[1] == "zip" {
-            name = "_zipped".to_string();
-        } else if args[1] == "unzip" {
-            name = "_unzipped".to_string();
-        } else {
-            return Err("First argument should be \"zip\" or \"unzip\".".to_string())
-        }
-
-        output_filename = format!("{}{}{}", &args[2][0..last_point], name, &args[2][last_point..]);
-    }
-    else if args.len() == 4 {
+        
+        output_filename = format!("{}{}{}", stem, name, ext);
+    } else if args.len() == 4 {
         output_filename = args[3].clone();
+    } else {
+        return Err("Incorrect number of arguments.".to_string());
     }
-    else{
-        return Err("Incorrect number of arguments.".to_string())
-    }
-
+    
     Ok(output_filename)
 }
 
@@ -40,7 +47,6 @@ fn main() -> Result<(), String> {
         Err(e) => return Err(e)
     };
 
-    
     
     if args[1] == "zip" {
         match zip_lib::zip(args[2].clone(), output_filename) {
